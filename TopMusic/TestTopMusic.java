8c86c129ae692410f72144c175f0309aec263568
Alberto Pérez Cano <Albertopc92@users.noreply.github.com>
2017-02-14 16:15:12 +0100

    Add files via upload

---
+++ TopMusic/TestTopMusic.java
@@ -0,0 +1,130 @@
+package listadoEstructurasDeDatos3.TopMusic;
+
+import utiles.Menu;
+import utiles.Teclado;
+
+/**
+ * TopMusic. Implementa un programa que gestione una lista de las canciones más
+ * escuchadas. El usuario podrá: a. Añadir una canción (en una posición) al
+ * TopMusic. b. Sacar un elemento del TopMusic. c. Subir un puesto en el
+ * TopMusic. d. Bajar un puesto en el TopMusic. e. Mostrar la lista TopMusic. f.
+ * Mostrar la canción más escuchada. Sobre la canción se almacenará el título,
+ * artista o grupo y año de grabación.
+ * 
+ * @author Alberto Perez Cano
+ * @version 1.0
+ */
+public class TestTopMusic {
+
+	static TopMusic topMusic = new TopMusic();
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Menu menu = new Menu("---------- TopMusic ----------", new String[] { "Añadir cancion", "Eliminar cancion",
+				"Subir puesto", "Bajar puesto", "Mostrar lista", "Mostrar cancion mas escuchada", "Mostrar top 10" });
+
+		do {
+			switch (menu.gestionar()) {
+			case 1:
+				addCancion();
+				break;
+			case 2:
+				delCancion();
+				break;
+			case 3:
+				upPosition();
+				break;
+			case 4:
+				downPosition();
+				break;
+			case 5:
+				show();
+				break;
+			case 6:
+				top();
+				break;
+			case 7:
+				top10();
+				break;
+			default:
+				System.out.println("Has salido del programa.");
+				return;
+			}
+		} while (true);
+	}
+
+	/**
+	 * Muestra el top 10 de la lista
+	 */
+	private static void top10() {
+		System.out.println(topMusic.top10());
+	}
+
+	/**
+	 * Muestra la cancion mas escuchada
+	 */
+	private static void top() {
+		if (topMusic.top() == null)
+			System.out.println("La lista esta vacia");
+		else
+			System.out.println(topMusic.top());
+	}
+
+	/**
+	 * Muestra el TopMusic
+	 */
+	private static void show() {
+		System.out.println(topMusic.show());
+	}
+
+	/**
+	 * Baja la posicion de una cancion
+	 */
+	private static void downPosition() {
+		if (topMusic.downPosition(Teclado.leerEntero("Indice de la cancion:")))
+			System.out.println("Bajo una posicion");
+		else
+			System.out.println("No se pudo realizar la operacion");
+	}
+
+	/**
+	 * Sube la posicion de una cancion en la lista
+	 */
+	private static void upPosition() {
+		if (topMusic.upPosition(Teclado.leerEntero("Indice de la cancion:")))
+			System.out.println("Subio una posicion");
+		else
+			System.out.println("No se ha podido realizar la operacion");
+	}
+
+	/**
+	 * Elimina una cancion de la lista
+	 */
+	private static void delCancion() {
+		if (topMusic.delCancion(Teclado.leerEntero("Introduce el indice de la cancion que desea eliminar: ")))
+			System.out.println("Se ha eliminado correctamente");
+		else
+			System.out.println("No se ha podido eliminar");
+	}
+
+	/**
+	 * Añade una cancion al topMusic
+	 */
+	private static void addCancion() {
+		int posicion = Teclado.leerEntero("Posicion:");
+		if (topMusic.posicionValida(posicion)) {
+			if (topMusic.addCancion(Teclado.leerCadena("Titulo:"), Teclado.leerCadena("Nombre del artista:"),
+					Teclado.leerEntero("Año:"), posicion))
+				System.out.println("Se ha añadido con exito");
+			else
+				System.out.println("No se ha podido añadir");
+		}else {
+			System.out.println("Posicion no valida");
+		}
+
+	}
+
+}
---
+++ TopMusic/TopMusic.java
@@ -0,0 +1,160 @@
+/**
+ * 
+ */
+package listadoEstructurasDeDatos3.TopMusic;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * @author Alberto Perez Cano
+ * @version 1.0
+ */
+public class TopMusic {
+	
+	private ArrayList<Cancion> topMusic = new ArrayList<Cancion>();
+	
+	/**
+	 * Comprueba si la posicion indicada por el usuario es correcta
+	 * @param index
+	 * 				Posicion introducida por el usuario
+	 * @return
+	 * 				false si no es correcta
+	 */
+	boolean posicionValida(int index) {
+		if(index-1 < 0 || index-1 > size())
+			return false;
+		return true;
+	}
+	
+	/**
+	 * Añade una cancion a la lista
+	 * @param titulo
+	 * 					Titulo de la cancion
+	 * @param artista
+	 * 					Artista de la cancion
+	 * @param anno
+	 * 					Año de la cancion
+	 * @param index
+	 * 					Posicion en la que añadir la cancion
+	 * @return
+	 * 					false si no se ha podido añadir la cancion
+	 */
+	boolean addCancion(String titulo, String artista, int anno, int index) {
+			if(posicionValida(index))
+				return false;
+			
+			Cancion cancion = new Cancion(titulo, artista, anno);
+			if(topMusic.contains(cancion))
+				return false;
+			topMusic.add(index - 1, cancion);
+			return true;
+	}
+	
+	/**
+	 * Elimina una cancion de la lista
+	 * @param index
+	 * 				Posicion de la cancion en la lista
+	 * @return
+	 * 				false si no se ha podido eliminar
+	 */
+	boolean delCancion(int index) {
+		if(isEmpty() || index-1 < 0 || index-1 > size())
+			return false;
+		if(topMusic.remove(topMusic.get(index-1)))
+			return true;
+		return false;
+	}
+	
+	/**
+	 * Sube la posicion de un elemento en el TopMusic
+	 * @param index
+	 * 				Elemento que va a subir una posicion
+	 * @return
+	 * 				false si no se ha podido realizar la operacion
+	 */
+	boolean upPosition(int index) {
+		if(isEmpty() || index-1 < 0 || index-1 > size() || index-1 < 1) {
+			return false;
+		}else {
+			topMusic.add(index - 2, topMusic.remove(index-1));
+			return true;
+		}
+	}
+	
+	/**
+	 * Baja la posicion de un elemento en el TopMusic
+	 * @param index
+	 * 				Elemento que va a bajar una posicion
+	 * @return
+	 * 				false si no se ha podido realizar la operacion
+	 */
+	boolean downPosition(int index) {
+		if(isEmpty() ||index-1 < 0 || index-1 > size() || index-1 > size()-1) {
+			return false;
+		}else {
+			topMusic.add((index-1) + 1, topMusic.remove(index-1));
+			return true;
+		}
+	}
+	
+	/**
+	 * Devuelve el valor del tamaño de la lista
+	 * @return
+	 * 			Tamaño de la lista
+	 */
+	private int size() {
+		return topMusic.size();
+	}
+	
+	/**
+	 * Muestra las 10 canciones mas escuchadas
+	 * @return
+	 */
+	StringBuilder top10() {
+		StringBuilder top10 = new StringBuilder();
+		
+		Iterator<Cancion> it = topMusic.iterator();
+		int i = 0;
+		while(it.hasNext()) {
+			top10.append(it.next().toString());
+			if(i++ == 9)
+				break;
+		}
+		
+		return top10;
+	}
+	
+	/**
+	 * Muestra la lista
+	 */
+	StringBuilder show() {
+		StringBuilder lista = new StringBuilder();
+		
+		Iterator<Cancion> it = topMusic.iterator();
+		while(it.hasNext()) {
+			lista.append(it.next().toString());
+		}
+		
+			return lista;
+	}
+	
+	/**
+	 * Muestra el elemento primero de la lista
+	 */
+	Cancion top() {
+		if(isEmpty())
+			return null;
+		return topMusic.get(0);
+	}
+	
+	/**
+	 * Comprueba si la lista esta vacia
+	 * @return
+	 * 			true si esta vacia
+	 */
+	private boolean isEmpty() {
+		return topMusic.isEmpty();
+	}
+	
+}
